819f0c49789fe59ab3db1f9323eb231b1dbf4cac
sndacq <seandacquel@gmail.com>
2020-09-03 19:56:04 +0800

    Added update via PUT

--- expense/api.py
+++ expense/api.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse
-from django.http import Http404, JsonResponse, HttpResponseBadRequest
+from django.http import Http404, JsonResponse,HttpResponseBadRequest
+from django.http.multipartparser import MultiPartParser
 from django.views.decorators.csrf import csrf_exempt
 from django import forms
 from rest_framework.decorators import api_view
@@ -19,6 +20,9 @@ def expense(request, expense_id=None):
 
     elif request.method == 'POST':
         return create_expense(request)
+
+    elif request.method == 'PUT':
+        return update_expense(request, expense_id)
 
     elif request.method == 'DELETE':
         return delete_expense(request, expense_id)
@@ -55,24 +59,25 @@ def get_expense(request, expense_id):
         print(error)
         raise Http404('Entry not found')
 
-
-class ExpenseValidationForm(forms.Form):
-    date = forms.DateTimeField()
-    amount = forms.DecimalField()
-    account = forms.IntegerField()
-    category = forms.IntegerField()
-    expense_type = forms.IntegerField()
-
-
 # TODO: figure out csrf on postman
 def create_expense(request):
+
+
+    class ExpenseValidationForm(forms.Form):
+        date = forms.DateTimeField()
+        amount = forms.DecimalField()
+        account = forms.IntegerField()
+        category = forms.IntegerField()
+        expense_type = forms.IntegerField()
+
+
     entry = ExpenseValidationForm(request.POST or None)
     if entry.is_valid():
         try:
             request_body = request.POST
 
             entry_account = Account.objects.get(pk=request_body['account'])
-            entry_category =  Category.objects.get(pk=request_body['category'])
+            entry_category = Category.objects.get(pk=request_body['category'])
 
             e = Expense(
                 date = request_body['date'],
@@ -89,6 +94,38 @@ def create_expense(request):
             return HttpResponseBadRequest('Unable to create entry')
     else:
         return HttpResponseBadRequest('Invalid form data')
+
+def update_expense(request, expense_id):
+    if expense_id:
+        try:
+            request_body = MultiPartParser(request.META, request, request.upload_handlers).parse()
+            request_body = request_body[0]
+
+            entry_account = Account.objects.get(pk=request_body['account'])
+            entry_category = Category.objects.get(pk=request_body['category'])
+            expense = Expense.objects.get(pk=expense_id)
+            expense.date = request_body['date']
+            expense.amount = request_body['amount']
+            expense.account = entry_account
+            expense.category = entry_category
+            expense.expense_type = request_body['expense_type']
+            expense.save()
+
+            return JsonResponse({
+                'id': expense.pk,
+                'date': expense.date,
+                'amount': expense.amount,
+                'expense_type': expense.expense_type,
+                'account': expense.account.pk,
+                'category': expense.category.pk,
+            })
+
+        except Exception as error:
+            print(error)
+            raise Http404('Unable to update entry')
+
+    else:
+        raise Http404('Entry not found')
 
 def delete_expense(request, expense_id):
     if expense_id:
